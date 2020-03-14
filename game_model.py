eb6926b8555ec61c78bc5ca3153c7c26f31f9f3f
Francesco Corda <35812424+francescocorda@users.noreply.github.com>
2020-03-14 17:50:36 +0100

    Initial draft of the game model

---
+++ game_model.py
@@ -0,0 +1,75 @@
+class PokerGame:
+    def __init__(self):
+        self.players = []
+        self.deck = []
+        self.tree = None
+        self.infoStructure = None
+        self.actions = []
+
+
+class Card:
+    def __init__(self):
+        self.cardName = None
+        self.value = None
+
+    def getValue(self):
+
+
+class Action:
+    def __init__(self):
+        self.name = None
+        self.probability = None
+
+
+class InfoStructure:  # list of information sets of player1 and player2
+    def __init__(self):
+        self.infoSets1 = []
+        self.infoSets2 = []
+
+
+class InfoSet:  # list of nodes for each information set
+    def __init__(self):
+        self.infoNodes = []
+
+
+class Node(object):
+    def __init__(self):
+        self.action = []
+        self.children = []
+        self.infoset = []
+        self.parent = None
+        self.card1 = None
+        self.card2 = None
+        self.card3 = None
+        self.payoff1 = None  # if terminal
+        self.payoff2 = None
+
+    def nextNode(action):
+
+    def getParent():
+
+    def getActions():
+
+    def getPlayer():
+
+    def getUtility():
+
+    def getCurrentNode():
+
+    def setNextNode(self, action):
+
+    def setParent(Tree):
+
+    def setActions():
+
+
+class NatureNode(Node):
+    def __init__(self):
+        self.children = []
+        self.parent = None
+
+
+class PlayerNode(Node):
+
+
+class TerminalNode(Node):
