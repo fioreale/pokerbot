088b0fa71add230afa6e17a5d6738232a43339bc
davided17 <37814114+davided17@users.noreply.github.com>
2020-03-16 17:55:02 +0100

    Update  - Chance Node  - Action Node  - Root Node

--- game_model.py
+++ game_model.py
@@ -32,21 +32,76 @@ class InfoSet:  # list of nodes for each information set
         self.infoNodes = []
 
 
-class Node(object):
-    def __init__(self):
-        self.action = []
-        self.children = []
+class Node:
+    def __init__(self, name):
+        self.name=name
+        self.actions = []
+        self.children = {}
         self.infoset = []
-        self.signals = []
+        self.signals = {}
         self.parent = None
-        self.card1 = None # possible to be added just on the root node without creating to much space for every node
-        self.card2 = None
-        self.card3 = None
-        self.payoff1 = None  # if terminal
-        self.payoff2 = None
+        #self.card1 = None # possible to be added just on the root node without creating to much space for every node
+        #self.card2 = None
+        #self.card3 = None
+        self.cards = []
+        self.payoffs = []
 
-    def setRoot(actions):
-        value = actions.split()
+    def node_finder(self, history):
+        if len(history) == 1:
+            return self.children[history[0]]
+        else:
+            for i in self.children.items():
+                if i.name == history[0]:
+                    return i.node_finder(i, history[1:])
+            return None
+
+    def setRoot(self, actions):
+        values = actions.split()
+        for i in values :
+            splitted_values = i.split('=')
+            self.children[splitted_values[0]] = Node('C:'+splitted_values[0])
+            self.children[splitted_values[0]].setCards(splitted_values[1])
+            self.signals[splitted_values[0]] = splitted_values[1]
+            self.actions.append(splitted_values[1])
+        sum = 0
+        for j in self.signals :
+            sum += self.signals[j]
+        for k in self.signals :
+            self.signals[k] = self.signals[k]/sum
+
+    def setActionNode(self, history, actions, root):
+        history_splitted = history.split('/')
+        self.parent = root.node_finder(history_splitted[1:])
+        actions_splitted = actions.split()
+        for i in actions_splitted :
+            self.actions.append(actions_splitted[i])
+        self.cards = self.parent.getCards()
+
+    def createChanceNode(self, history, actions, root):
+        history_splitted = history.split('/')
+        self.parent = root.node_finder(history_splitted[1:])
+        actions_splitted = actions.split()
+        for i in actions_splitted:
+            splitted_values = i.split('=')
+            self.children[splitted_values[0]] = Node('C:' + splitted_values[0])
+            self.children[splitted_values[0]].setCards(splitted_values[1])
+            self.signals[splitted_values[0]] = splitted_values[1]
+            self.actions.append(splitted_values[1])
+        sum = 0
+        for j in self.signals:
+            sum += self.signals[j]
+        for k in self.signals:
+            self.signals[k] = self.signals[k] / sum
+
+
+    def appendCards(self, cards):
+        self.cards.append(cards)
+
+    def getCards(self):
+        return self.cards
+
+    def setCards(self, cards):
+        self.cards = cards
 
     def nextNode(action):
 
@@ -77,3 +132,5 @@ class PlayerNode(Node):
 
 
 class TerminalNode(Node):
+
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import Input_file_parser as NewP
+#import Input_file_parser as NewP
 
 if __name__ == '__main__':
-    print(NewP.parse_line('node /C:JQ player 1 actions c r'))
-    asd = 'JK=1.000000 QJ=1.000000 QK=1.000000 KJ=1.000000'
-    print(asd.split())
+    #print(NewP.parse_line('node /C:JQ player 1 actions c r'))
+    #asd = 'JK=1.000000 QJ=1.000000 QK=1.000000 KJ=1.000000'
+    #print(asd.split())
+    a = ['99=2.000', 'KJ=1.000']
+    for i in a:
+        splitted_values = i.split('=')
+        print(splitted_values)
