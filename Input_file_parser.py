b49705ffe02cfb788dbd930af2a6ed41695c5f69
Francesco Corda <35812424+francescocorda@users.noreply.github.com>
2020-03-16 21:50:07 +0100

    Update in the parser and the game_model, added input files

--- Input_file_parser.py
+++ Input_file_parser.py
@@ -1,21 +1,21 @@
 # 1 reading the input file from txt to a raw
-
+import os
 import re
 
 # set up regular expressions
 # use https://regexper.com to visualise these if required
 import game_model
+from game_model import Node
 
 rx_dict = {
     #'action_node': re.compile(r'node /(?P<chance1>C.*?)(?P<history1>/.*?)?(/(?P<chance2>(C.*?))(?P<history2>(/.*?)))? (?P<player>.*( )?.*?) actions (?P<actions>.*)'),
     #'chance_node': re.compile(r'node /(?P<chance1>C.*?)(?P<history1>/.*?)(/(?P<chance2>(C.*?))(?P<history2>(/.*?)))? chance actions (?P<actions>.*)'),
     #'leaf_node': re.compile(r'node /(?P<chance1>C.*?)(?P<history1>/.*?)(/(?P<chance2>(C.*?))(?P<history2>(/.*?)))? leaf payoffs (?P<payoffs>.*)'),
     'root_node': re.compile(r'node / chance actions (?P<actions>.*)'),
-    'infoset': re.compile(r'infoset (?P<history>.*?) nodes (?P<nodes>.*)'),
-
+    'action_node': re.compile(r'node (?P<history>.*?) (?P<player>.*( )?.*?) actions (?P<actions>.*)'),
     'chance_node': re.compile(r'node (?P<history>.*?) chance actions (?P<actions>.*)'),
-    'action_node': re.compile(r'node (?P<history>.*?) actions (?P<actions>.*)'),
     'leaf_node': re.compile(r'node (?P<history>.*?) leaf payoffs (?P<payoffs>.*)'),
+    'infoset': re.compile(r'infoset (?P<history>.*?) nodes (?P<nodes>.*)')
 }
 
 
@@ -53,84 +53,51 @@ def parse_file(filepath):
     data = []  # create an empty list to collect the data
     # open the file and read through it line by line
     with open(filepath, 'r') as file_object:
-        line = file_object.readline()
-        while line:
+        lines = file_object.readlines()
+        root = game_model.Node()
+        for line in reversed(lines):
             # at each line check for a match with a regex
             key, match = parse_line(line)
-
             # extract school name
             if key == 'root_node':
                 actions = match.group('actions')
-                root = game_model.Node.setRoot(actions)
+                root.createRootNode(actions)
 
             if key == 'action_node':
                 history = match.group('history')
                 actions = match.group('actions')
-                game_model.Node.setActionNode(history, actions, root)
-
-
-            if key == 'history_node':
-                chance1 = match.group('chance1')
-                history1 = match.group('history1')
-                chance2 = match.group('chance2')
-                history2 = match.group('history2')
                 player = match.group('player')
-                actions = match.group('arguments')
+                action_node = game_model.Node()
+                action_node.createActionNode(history, player, actions, root)
 
             if key == 'chance_node':
+                history = match.group('history')
                 actions = match.group('actions')
-                game_model.Node.createChanceNode(history, actions, root)
+                chance_node = game_model.Node()
+                chance_node.createChanceNode(history, actions, root)
 
             if key == 'leaf_node':
+                history = match.group('history')
+                actions = match.group('actions')
+                leaf_node = game_model.Node()
+                leaf_node.createLeafNode(history, actions, root)
 
             # if key == 'infoset':
             #     history = match.group('history')
             #     arguments = match.group('arguments')
-
-
-
-            # # identify a table header
-            # if key == 'name_score':
-            #     # extract type of table, i.e., Name or Score
-            #     value_type = match.group('name_score')
-            #     line = file_object.readline()
-            #     # read each line of the table until a blank line
-            #     while line.strip():
-            #         # extract number and value
-            #         number, value = line.strip().split(',')
-            #         value = value.strip()
-            #         # create a dictionary containing this row of data
-            #         row = {
-            #             'School': school,
-            #             'Grade': grade,
-            #             'Student number': number,
-            #             value_type: value
-            #         }
-            #         # append the dictionary to the data list
-            #         data.append(row)
-            #         line = file_object.readline()
-
-            line = file_object.readline()
-
-        # create a pandas DataFrame from the list of dicts
-        data = pd.DataFrame(data)
-        # set the School, Grade, and Student number as the index
-        data.set_index(['School', 'Grade', 'Student number'], inplace=True)
-        # consolidate df to remove nans
-        data = data.groupby(level=data.index.names).first()
-        # upgrade Score from float to integer
-        data = data.apply(pd.to_numeric, errors='ignore')
-    return data
+    return root
 
 
 if __name__ == '__main__':
-    key, match = parse_line('node /C:JQ player 1 actions c r')
-    print(key)
-    print(match)
-    print(match.group('history'))
-    key, match = parse_line('infoset /C:J?/P1:c/P2:r nodes /C:JQ/P1:c/P2:r /C:JK/P1:c/P2:r')
-    print(key)
-    print(match)
-    key, match = parse_line('infoset /C:J? nodes /C:JQ /C:JK')
-    print(key)
-    print(match)
+    # key, match = parse_line('node /C:JQ player 1 actions c r')
+    # print(key)
+    # print(match)
+    # print(match.group('history'))
+    # key, match = parse_line('infoset /C:J?/P1:c/P2:r nodes /C:JQ/P1:c/P2:r /C:JK/P1:c/P2:r')
+    # print(key)
+    # print(match)
+    # key, match = parse_line('infoset /C:J? nodes /C:JQ /C:JK')
+    # print(key)
+    # print(match)
+
+    tree = parse_file(os.path.join(os.getcwd(), 'inputs', 'leduc5.txt'))
--- game_model.py
+++ game_model.py
@@ -1,136 +1,140 @@
-class PokerGame:
-    def __init__(self):
-        self.players = []
-        self.deck = []
-        self.tree = None
-        self.infoStructure = None
-        self.actions = []
-
-
-class Card:
-    def __init__(self):
-        self.cardName = None
-        self.value = None
-
-    def getValue(self):
-
-
-class Action:
-    def __init__(self):
-        self.name = None
-        self.probability = None
-
-
-class InfoStructure:  # list of information sets of player1 and player2
-    def __init__(self):
-        self.infoSets1 = []
-        self.infoSets2 = []
-
-
-class InfoSet:  # list of nodes for each information set
-    def __init__(self):
-        self.infoNodes = []
+# class PokerGame:
+#     def __init__(self):
+#         self.players = []
+#         self.deck = []
+#         self.tree = None
+#         self.infoStructure = None
+#         self.actions = []
+#
+#
+# class Card:
+#     def __init__(self):
+#         self.cardName = None
+#         self.value = None
+#
+#     def getValue(self):
+#
+#
+# class Action:
+#     def __init__(self):
+#         self.name = None
+#         self.probability = None
+#
+#
+# class InfoStructure:  # list of information sets of player1 and player2
+#     def __init__(self):
+#         self.infoSets1 = []
+#         self.infoSets2 = []
+#
+#
+# class InfoSet:  # list of nodes for each information set
+#     def __init__(self):
+#         self.infoNodes = []
 
 
 class Node:
-    def __init__(self, name):
-        self.name=name
-        self.actions = []
+    def __init__(self):
+        self.history = []
         self.children = {}
+        self.actions = []
         self.infoset = []
         self.signals = {}
         self.parent = None
-        #self.card1 = None # possible to be added just on the root node without creating to much space for every node
-        #self.card2 = None
-        #self.card3 = None
         self.cards = []
-        self.payoffs = []
+        self.payoffs = {}
 
     def node_finder(self, history):
         if len(history) == 1:
-            return self.children[history[0]]
+            return self
         else:
-            for i in self.children.items():
-                if i.name == history[0]:
+            for i in self.children.values():
+                if i.history[-1] == history[0]:
                     return i.node_finder(i, history[1:])
             return None
 
-    def setRoot(self, actions):
+    def appendChild(self, node):
+        self.children(node)
+
+    def createRootNode(self, actions):
+        self.history = []
         values = actions.split()
         for i in values :
-            splitted_values = i.split('=')
-            self.children[splitted_values[0]] = Node('C:'+splitted_values[0])
-            self.children[splitted_values[0]].setCards(splitted_values[1])
-            self.signals[splitted_values[0]] = splitted_values[1]
-            self.actions.append(splitted_values[1])
+            splitted_signals = i.split('=')
+            #self.children[splitted_values[0]] = Node('C:'+ splitted_values[0])
+            #self.children[splitted_values[0]].setCards(splitted_values[1])
+            self.signals[splitted_signals[0]] = float(splitted_signals[1])
+            self.actions.append(splitted_signals[1])
         sum = 0
         for j in self.signals :
             sum += self.signals[j]
         for k in self.signals :
-            self.signals[k] = self.signals[k]/sum
+            self.signals[k] = self.signals[j]/sum
+        return self
 
-    def setActionNode(self, history, actions, root):
+    def createActionNode(self, history, player, actions, root):
         history_splitted = history.split('/')
+        self.history = history_splitted
         self.parent = root.node_finder(history_splitted[1:])
+        self.parent.appendChild(self)
         actions_splitted = actions.split()
         for i in actions_splitted :
             self.actions.append(actions_splitted[i])
         self.cards = self.parent.getCards()
+        return self
 
     def createChanceNode(self, history, actions, root):
         history_splitted = history.split('/')
+        self.history = history_splitted
         self.parent = root.node_finder(history_splitted[1:])
-        actions_splitted = actions.split()
-        for i in actions_splitted:
-            splitted_values = i.split('=')
-            self.children[splitted_values[0]] = Node('C:' + splitted_values[0])
-            self.children[splitted_values[0]].setCards(splitted_values[1])
-            self.signals[splitted_values[0]] = splitted_values[1]
-            self.actions.append(splitted_values[1])
+        self.parent.appendChild(self)
+        actions_list = actions.split()
+        for i in actions_list:
+            splitted_action = i.split('=')
+            #self.children[splitted_values[0]] = Node()
+            #self.children[splitted_values[0]].setCards(splitted_values[1])
+            self.signals[splitted_action[0]] = splitted_action[1]
+            self.actions.append(splitted_action[1])
         sum = 0
         for j in self.signals:
             sum += self.signals[j]
         for k in self.signals:
             self.signals[k] = self.signals[k] / sum
+        return self
 
-
+    def createLeafNode(self, history, payoffs, root):
+        history_splitted = history.split('/')
+        self.history = history_splitted
+        self.parent = root.node_finder(history_splitted[1:])
+        self.parent.appendChild(self)
+        payoffs_list = payoffs.split()
+        for i in payoffs_list:
+            splitted_payoffs = i.split('=')
+            self.payoffs[splitted_payoffs[0]]=splitted_payoffs[1]
+        return self
     def appendCards(self, cards):
         self.cards.append(cards)
 
     def getCards(self):
         return self.cards
 
+
     def setCards(self, cards):
         self.cards = cards
 
-    def nextNode(action):
-
-    def getParent():
-
-    def getActions():
-
-    def getPlayer():
-
-    def getUtility():
-
-    def getCurrentNode():
-
-    def setNextNode(self, action):
-
-    def setParent(Tree):
-
-    def setActions():
-
-
-class NatureNode(Node):
-    def __init__(self):
-        self.children = []
-        self.parent = None
-
-
-class PlayerNode(Node):
-
-
-class TerminalNode(Node):
-
-
+    # def nextNode(action):
+    #
+    # def getParent():
+    #
+    # def getActions():
+    #
+    # def getPlayer():
+    #
+    # def getUtility():
+    #
+    # def getCurrentNode():
+    #
+    # def setNextNode(self, action):
+    #
+    # def setParent(Tree):
+    #
+    # def setActions():
