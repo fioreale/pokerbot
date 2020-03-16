47b256d8d994884bee20284f930fd6a759cd5d93
Alexio96 <10495770@polimi.it>
2020-03-16 15:29:30 +0100

    Update Parser  - beginning implementation of built functions

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import Input_file_parser as NewP
 
 if __name__ == '__main__':
     print(NewP.parse_line('node /C:JQ player 1 actions c r'))
+    asd = 'JK=1.000000 QJ=1.000000 QK=1.000000 KJ=1.000000'
+    print(asd.split())
