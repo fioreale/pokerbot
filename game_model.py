771dc1ebb7a71707f04a796249c6738be7e34496
Alexio96 <10495770@polimi.it>
2020-03-17 12:54:04 +0100

    Update Parser  - Inheritance added in nodes

--- game_model.py
+++ game_model.py
@@ -1,140 +1,133 @@
-# class PokerGame:
-#     def __init__(self):
-#         self.players = []
-#         self.deck = []
-#         self.tree = None
-#         self.infoStructure = None
-#         self.actions = []
-#
-#
+import Utilities
+
+class PokerGame:
+    def __init__(self):
+        #self.players = []
+        self.deck = []
+        self.tree = None    # just one Nature Player
+        self.infoStructure = None   # list of infosets for each single player
+        self.actions = []   # list of possible actions, depending on the type of poker
+
 # class Card:
 #     def __init__(self):
 #         self.cardName = None
 #         self.value = None
 #
 #     def getValue(self):
-#
-#
-# class Action:
-#     def __init__(self):
-#         self.name = None
-#         self.probability = None
-#
-#
-# class InfoStructure:  # list of information sets of player1 and player2
-#     def __init__(self):
-#         self.infoSets1 = []
-#         self.infoSets2 = []
-#
-#
-# class InfoSet:  # list of nodes for each information set
-#     def __init__(self):
-#         self.infoNodes = []
+
+class Action:
+    def __init__(self):
+        self.name = None
+        self.probability = None
+
+
+class InfoStructure:  # list of information sets of player1 and player2
+    def __init__(self):
+        self.infoSets1 = []
+        self.infoSets2 = []
+
+
+class InfoSet:  # list of nodes for each information set
+    def __init__(self):
+        self.infoNodes = []
 
 
 class Node:
-    def __init__(self):
-        self.history = []
+    def __init__(self, name):
+        self.name=name
         self.children = {}
-        self.actions = []
-        self.infoset = []
-        self.signals = {}
+        #self.infoset = [] maybe it should be referenced a part, and outside the nodes, since an infoset could include nodes of many subtrees
         self.parent = None
         self.cards = []
-        self.payoffs = {}
+
+    def appendCards(self, cards):
+        if len(self.cards)>1:
+            self.cards.append(Utilities.in_chars(cards[0]))
+        else:
+            return ValueError("No cards in the player hand")
+
+    def getCards(self):
+        return self.cards
+
+    def setCards(self, cards):
+        self.cards = Utilities.in_chars(cards)
+
+    def nextNode(action):
+
+    def getParent():
+
+    def getCurrentNode():
+
+    def setNextNode(self, action):
+
+    def setParent(Tree):
 
     def node_finder(self, history):
         if len(history) == 1:
-            return self
+            return self.children[history[0]]
         else:
-            for i in self.children.values():
-                if i.history[-1] == history[0]:
+            for i in self.children.items():
+                if i.name == history[0]:
                     return i.node_finder(i, history[1:])
             return None
 
-    def appendChild(self, node):
-        self.children(node)
+class NatureNode(Node):
+    def __init__(self,name):
+        super.__init__(name)
+        self.signals = {}
 
-    def createRootNode(self, actions):
-        self.history = []
+    def setRoot(self, actions):
         values = actions.split()
         for i in values :
-            splitted_signals = i.split('=')
-            #self.children[splitted_values[0]] = Node('C:'+ splitted_values[0])
-            #self.children[splitted_values[0]].setCards(splitted_values[1])
-            self.signals[splitted_signals[0]] = float(splitted_signals[1])
-            self.actions.append(splitted_signals[1])
-        sum = 0
+            splitted_values = i.split('=')
+            self.children[splitted_values[0]] = Node('C:'+splitted_values[0])
+            self.children[splitted_values[0]].setCards(splitted_values[0])
+            self.signals[splitted_values[0]] = splitted_values[1]
+            self.actions.append(splitted_values[1])
         for j in self.signals :
             sum += self.signals[j]
         for k in self.signals :
-            self.signals[k] = self.signals[j]/sum
-        return self
-
-    def createActionNode(self, history, player, actions, root):
-        history_splitted = history.split('/')
-        self.history = history_splitted
-        self.parent = root.node_finder(history_splitted[1:])
-        self.parent.appendChild(self)
-        actions_splitted = actions.split()
-        for i in actions_splitted :
-            self.actions.append(actions_splitted[i])
-        self.cards = self.parent.getCards()
-        return self
+            self.signals[k] = self.signals[k]/sum
 
     def createChanceNode(self, history, actions, root):
         history_splitted = history.split('/')
-        self.history = history_splitted
         self.parent = root.node_finder(history_splitted[1:])
-        self.parent.appendChild(self)
-        actions_list = actions.split()
-        for i in actions_list:
-            splitted_action = i.split('=')
-            #self.children[splitted_values[0]] = Node()
-            #self.children[splitted_values[0]].setCards(splitted_values[1])
-            self.signals[splitted_action[0]] = splitted_action[1]
-            self.actions.append(splitted_action[1])
+        actions_splitted = actions.split()
+        for i in actions_splitted:
+            splitted_values = i.split('=')
+            self.children[splitted_values[0]] = Node('C:' + splitted_values[0])
+            self.children[splitted_values[0]].setCards(splitted_values[1])
+            self.signals[splitted_values[0]] = splitted_values[1]
+            self.actions.append(splitted_values[1])
         sum = 0
         for j in self.signals:
             sum += self.signals[j]
         for k in self.signals:
             self.signals[k] = self.signals[k] / sum
-        return self
 
-    def createLeafNode(self, history, payoffs, root):
+
+class ActionNode(Node):
+    def __init__(self,name):
+        super.__init__(name)
+        self.actions = []
+
+    def getActions():
+
+    def setActions():
+
+    def setActionNode(self, history, actions, root):
         history_splitted = history.split('/')
-        self.history = history_splitted
         self.parent = root.node_finder(history_splitted[1:])
-        self.parent.appendChild(self)
-        payoffs_list = payoffs.split()
-        for i in payoffs_list:
-            splitted_payoffs = i.split('=')
-            self.payoffs[splitted_payoffs[0]]=splitted_payoffs[1]
-        return self
-    def appendCards(self, cards):
-        self.cards.append(cards)
-
-    def getCards(self):
-        return self.cards
+        actions_splitted = actions.split()
+        for i in actions_splitted :
+            self.actions.append(actions_splitted[i])
+        self.cards = self.parent.getCards()
 
 
-    def setCards(self, cards):
-        self.cards = cards
+class TerminalNode(Node):
+    def __init__(self, name):
+        super.__init__(name)
+        self.payoffs = []
 
-    # def nextNode(action):
-    #
-    # def getParent():
-    #
-    # def getActions():
-    #
-    # def getPlayer():
-    #
-    # def getUtility():
-    #
-    # def getCurrentNode():
-    #
-    # def setNextNode(self, action):
-    #
-    # def setParent(Tree):
-    #
-    # def setActions():
+    def getUtility():
+
