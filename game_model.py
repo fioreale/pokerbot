6ff35e721ffa65120eb1228c8819b95c7f57dea9
fioreale <alefio96@gmail.com>
2020-03-19 23:46:57 +0100

    Update Utilities  - Added a new visualizer for the tree

--- game_model.py
+++ game_model.py
@@ -20,6 +20,30 @@
 #         self.name = None
 #         self.probability = None
 
+# def print_tree(self, level):
+#     height = 0
+#     while height < level:
+#         print('|    ', end='')
+#         height += 1
+#     if len(self.history) > 0:
+#         print(self.history[-1])
+#     else:
+#         print('C')
+#     for i in self.children.values():
+#         i.print_tree(level + 1)
+#
+#     def print_tree(self, level):                                    # override of the parent function to print payoffs
+#         height = 0
+#         while height < level:
+#             print('|    ', end='')
+#             height += 1
+#
+#         player = 1
+#         for payoff in self.payoffs.values():
+#             print(str(player) + '=' + str(payoff), end=' ')
+#             player += 1
+#         print('\n')
+#
 import utilities
 
 
--- utilities.py
+++ utilities.py
@@ -1,2 +1,32 @@
+from tree_elements.terminal_node import TerminalNode
+
 def in_chars(string):
     return [char for char in string]
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def visualize(el, level):
+    if el.children is not None:
+        for key, value in el.children.items():
+            print('{:<15}'.format(value.history[-1] + '  ->'), end='')
+            visualize(value, level+1)
+            player = 1
+            if isinstance(value, TerminalNode):
+                for payoff in value.payoffs.values():
+                    print('{:^10}'.format(bcolors.HEADER + bcolors.BOLD + '(' + str(player) + '=' + str(payoff) + ')' + bcolors.ENDC), end=' ')
+                    player = 2
+            num = 0
+            print('')
+            while num < level:
+                print('{:>15}'.format(' |'), end='')
+                num += 1
+    else:
+        return
