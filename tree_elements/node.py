66c4d2917159953aae5a0e8dacc808fce98791a0
LFK01 <37867917+LFK01@users.noreply.github.com>
2020-03-21 12:44:30 +0100

    Build of cluster table:  - creation of compute_utilities() function that return the backward induction values  - clustering_table_creation() function

--- tree_elements/node.py
+++ tree_elements/node.py
@@ -9,6 +9,10 @@ class Node:
         self.parent = None
         self.cards = []
         self.infoSet = None
+        self.player = None
+        self.utilities = {} # utilities dictionary used  to compute backward induction outcomes
+        self.utilities_per_action = {} # dictionary where we save all the possible utilities for each single action
+                                        # reference Abstraction_Generation.pdf slide 23/67
 
     def node_finder(self, history):     # recursive function that traverse the history and returns its last node
         if len(history) == 0:
@@ -30,3 +34,23 @@ class Node:
 
     def setCards(self, cards):
         self.cards = cards
+
+    def compute_utilities(self):
+        # cycle through each action to retrieve the child node utilities
+        # returned utilities are a list [utility_player_1, utility_player_2]
+        for action in self.actions:
+            self.utilities[action] = self.children[self.player + ':' + action].compute_utilities()
+        # initialization of the list max_utility where  we're gonna save the best utility for this node
+        max_utility = self.utilities[self.actions[0]]
+        # cycle through the computed utilities to compute the best one
+        for utility in self.utilities.items():
+            # computation of best utility for first player
+            if self.player == '1':
+                if utility[0] > max_utility[0]:
+                    max_utility = utility
+            # computation of best utility for second player
+            if self.player == '2':
+                if utility[1] > max_utility[1]:
+                    max_utility = utility
+        # returns best utility which is a vector [utility_player_1, utility_player_2]
+        return max_utility
