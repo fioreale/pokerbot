91106c3225eb7168347c20578c402267d4860180
LFK01 <37867917+LFK01@users.noreply.github.com>
2020-03-24 13:35:41 +0100

    Update:  - variable renaming in kmeans_calculator.py for code clarification

--- working_tools/kmeans_calculator.py
+++ working_tools/kmeans_calculator.py
@@ -2,87 +2,91 @@ from sklearn.cluster import KMeans
 import numpy as np
 
 
-def k_means(table, clusters):
+def k_means(cluster_table, number_of_clusters):
     # utility list where we store the "coordinates" of the nodes to cluster
-    utilities = []
+    utilities_coordinates_list = []
     # nodes list where we store the nodes in the same order of the utilities list
     # in order to be able to achieve the node of the corresponding coordinates
-    nodes = []
+    node_ordered_list = []
     # list of action where we save the distinct values of actions ordered to create the coordinates in the same order
-    actions = []
+    distinct_actions_order = []
 
     # cycle to create the list of ordered distinct values of actions
-    for action, utility in table.keys():
-        if action not in actions:
-            actions.append(action)
+    for action, utility in cluster_table.keys():
+        if action not in distinct_actions_order:
+            distinct_actions_order.append(action)
 
     # here comes the deep shit written by luciano
     # cycle through the values of the dictionary which are list of nodes
-    for node_list in table.values():
+    for node_list in cluster_table.values():
         # cycle through the nodes inside each list of the node lists
-        for node in node_list:
+        for current_node in node_list:
             # list where to save the utilities ordered by action
-            values = []
+            values_for_coordinates = []
             # cycle through the ordered list of action to search the exact utility corresponding to that action
-            for action_of_the_coordinate in actions:
+            for single_action_from_order in distinct_actions_order:
                 # cycle again through the dictionary to retrieve the utilities for the selected action
-                for (action, utility), node_list_second in table.items():
+                for (action_second_cycle, utility_second_cycle), node_list_second_cycle in cluster_table.items():
                     # check if the action in the dictionary corresponds to the selected action
-                    if action == action_of_the_coordinate:
+                    if action_second_cycle == single_action_from_order:
                         # check if the node of the searched action corresponds to the action in the cycled dictionary
-                        if node in node_list_second:
+                        if current_node in node_list_second_cycle:
                             # then we append the utility value to the list where we save the coordinates
-                            values.append(float(utility)/1000000)
+                            values_for_coordinates.append(float(utility_second_cycle)/1000000)
             # the single coordinate has dimension equal to the number of actions that the player can do
             # dim[values] = dim[actions]
             # then we append the coordinate to the coordinate list
-            utilities.append(values)
+            utilities_coordinates_list.append(values_for_coordinates)
             # append the node of the corresponding coordinate just found to the nodes list in the same order
-            nodes.append(node)
+            node_ordered_list.append(current_node)
 
     # here we build a dictionary indexed for each infoset storing the list of nodes and coordinate values
     # in that infoset
-    infosets = {}
+    infosets_dictionary = {}
     # cycle through the nodes
-    for node_list in table.values():
-        for node in node_list:
+    for node_list in cluster_table.values():
+        for current_node in node_list:
             # create list of two values = node object, utilities coordinates
-            tuple_node_utilities = [None, None]
-            tuple_node_utilities[0] = node
-            tuple_node_utilities[1] = utilities[nodes.index(node)]
+            couple_node_coordinates = [None, None]
+            # save current node in the tuple
+            couple_node_coordinates[0] = current_node
+            # retrieve coordinates of the current node
+            desired_utilities_coordinates = utilities_coordinates_list[node_ordered_list.index(current_node)]
+            # save coordinates in the tuple
+            couple_node_coordinates[1] = desired_utilities_coordinates
             # creation of a new entry in the dictionary and store the new value
-            if node.infoset.name not in infosets.keys():
-                infosets[node.infoset.name] = []
-                infosets[node.infoset.name].append(tuple_node_utilities)
+            if current_node.infoset.name not in infosets_dictionary.keys():
+                infosets_dictionary[current_node.infoset.name] = []
+                infosets_dictionary[current_node.infoset.name].append(couple_node_coordinates)
             else:
-                infosets[node.infoset.name].append(tuple_node_utilities)
+                infosets_dictionary[current_node.infoset.name].append(couple_node_coordinates)
 
     # initialization of centroids structure = dictionary indexed by infoset object and storing the values
     # of the cluster coordinates
-    centroids = {}
+    centroids_dictionary = {}
     # cycle through infosets dictionary
-    for infoset in infosets.keys():
+    for infoset in infosets_dictionary.keys():
         # initialization of action_utilities list where we store the utilities of the single infoset
         actions_utilities = []
         # cycle through the dictionary to retrieve the utilities
-        for tuple in infosets[infoset]:
-            actions_utilities.append(tuple[1])
+        for node_coordinates_couple in infosets_dictionary[infoset]:
+            actions_utilities.append(node_coordinates_couple[1])
         # build numpy array of utilities for the infosets
         kmeans_input = np.asarray(actions_utilities)
         # call K-Means on the utilities for the single infoset
         kmeans = KMeans(n_clusters=1, random_state=0).fit(kmeans_input)
         # storing the cluster coordinates in the centroids dictionary
-        centroids[infoset] = kmeans.cluster_centers_
+        centroids_dictionary[infoset] = kmeans.cluster_centers_
 
     # list used to convert the centroids dictionary into an array
     centroids_values = []
     # cycle through the coordinates of centroids in the centroids dictionary
-    for value in centroids.values():
+    for value in centroids_dictionary.values():
         # conversion of values
         centroids_values.append((float(value[0][0]), float(value[0][1])))
     # build numpy array of centroids coordinates
     kmeans_input = np.asarray(centroids_values)
     # compute the K-Means of the centroids of the infoset clusters
-    kmeans = KMeans(n_clusters=clusters, random_state=0).fit(kmeans_input)
+    kmeans = KMeans(n_clusters=number_of_clusters, random_state=0).fit(kmeans_input)
     # return the coordinates of the new centroids
     return kmeans.cluster_centers_
